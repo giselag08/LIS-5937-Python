214eb8b62c935a6d7bdf97486d817d0ec3cf12ae
Gisela Gonzalez <77498589+giselag08@users.noreply.github.com>
2021-02-12 14:05:05 -0500

    Update module5_assignment.py

--- module5_assignment.py
+++ module5_assignment.py
@@ -3,9 +3,9 @@
 'GonZAlez'.upper()
 "I am new to programming".split()
 ''.join(['I', 'am','new','to','programming'])
-# Write a Python function to insert string in the middle of a string 
 'Python rocks'.find('oc')
 'Python rocks'.replace('rocks','cool')
+# Write a Python function to insert string in the middle of a string 
 line = 'UF'
 index = line.find('S')
 output_line = line[:index] + 'S' + line[index:]
