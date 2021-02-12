72539a64137e7e6f2e5356efa5e27b949866f21a
Gisela Gonzalez <77498589+giselag08@users.noreply.github.com>
2021-02-12 14:01:01 -0500

    Add files via upload

---
+++ module5_assignment.py
@@ -0,0 +1,11 @@
+# Write a Python program to execute a string containing Python code
+'GonZAlEZ'.lower()
+'GonZAlez'.upper()
+"I am new to programming".split()
+''.join(['I', 'am','new','to','programming'])
+'Python rocks'.find('oc')
+'Python rocks'.replace('rocks','cool')
+line = 'UF'
+index = line.find('S')
+output_line = line[:index] + 'S' + line[index:]
+output_line
